64f8c199cc1dd16e3383d6f966fb7d445eb9d94c
d-murphy <murphy.dan.s@gmail.com>
2022-06-08 22:02:39 -0400

    added more data

--- src/services/database/tracksMock.ts
+++ src/services/database/tracksMock.ts
@@ -61,6 +61,126 @@ function loadMockTracks(): Track[]{
             imageUrls: [], 
             archHeight: null,
             distanceToHydrant: 200
+        },
+        {
+            id: 3, 
+            name: "Lindenhurst", 
+            address: "", 
+            city: "Lindenhurst", 
+            notes: "",
+            imageUrls: [], 
+            archHeight: null,
+            distanceToHydrant: 200
+        },
+        {
+            id: 4, 
+            name: "Merrick", 
+            address: "", 
+            city: "Merrick", 
+            notes: "",
+            imageUrls: [], 
+            archHeight: null,
+            distanceToHydrant: null
+        },
+        {
+            id: 5, 
+            name: "Ridge", 
+            address: "", 
+            city: "Ridge", 
+            notes: "",
+            imageUrls: [], 
+            archHeight: null,
+            distanceToHydrant: null
+        },
+        {
+            id: 6, 
+            name: "Hempstead", 
+            address: "", 
+            city: "Hempstead", 
+            notes: "",
+            imageUrls: [], 
+            archHeight: null,
+            distanceToHydrant: null
+        },
+        {
+            id: 7, 
+            name: "Copiague", 
+            address: "", 
+            city: "Copiague", 
+            notes: "",
+            imageUrls: [], 
+            archHeight: null,
+            distanceToHydrant: null
+        },
+        {
+            id: 9, 
+            name: "Point Pleasant", 
+            address: "", 
+            city: "Point Pleasant", 
+            notes: "",
+            imageUrls: [], 
+            archHeight: null,
+            distanceToHydrant: null
+        },
+        {
+            id: 10, 
+            name: "Deerfield", 
+            address: "", 
+            city: "Deerfield", 
+            notes: "",
+            imageUrls: [], 
+            archHeight: null,
+            distanceToHydrant: null
+        },
+        {
+            id: 11, 
+            name: "Main-Transit", 
+            address: "", 
+            city: "Main-Transit", 
+            notes: "",
+            imageUrls: [], 
+            archHeight: null,
+            distanceToHydrant: null
+        },
+        {
+            id: 12, 
+            name: "Spencerport", 
+            address: "", 
+            city: "Spencerport", 
+            notes: "",
+            imageUrls: [], 
+            archHeight: null,
+            distanceToHydrant: null
+        },
+        {
+            id: 13, 
+            name: "Bayville", 
+            address: "", 
+            city: "Bayville", 
+            notes: "",
+            imageUrls: [], 
+            archHeight: null,
+            distanceToHydrant: null
+        },
+        {
+            id: 14, 
+            name: "Baldwin", 
+            address: "", 
+            city: "Baldwin", 
+            notes: "",
+            imageUrls: [], 
+            archHeight: null,
+            distanceToHydrant: null
+        },
+        {
+            id: 15, 
+            name: "West Sayville", 
+            address: "", 
+            city: "West Sayville", 
+            notes: "",
+            imageUrls: [], 
+            archHeight: null,
+            distanceToHydrant: null
         }
 
     
